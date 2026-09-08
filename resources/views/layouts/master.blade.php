1c5219b9d0188c17d4c1f84788af75174a8443d7
yecarter13 <pouabegille@gmail.com>
2026-09-08 11:02:10 +0100

    fix: liens CSS/JS en dur pour eviter cache Vite

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -79,7 +79,8 @@
     }
     </script>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('build/assets/app-RujeqNfL.css') }}">
+    <script type="module" src="{{ asset('build/assets/app-BvRk9kiK.js') }}"></script>
 </head>
 <body class="font-sans antialiased bg-white text-stable-900">
 
